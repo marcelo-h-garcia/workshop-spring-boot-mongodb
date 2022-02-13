1c5318bae8c2675b6b8ea17452aaec958086401f
Marcelo <marceloph2@yahoo.com.br>
2022-02-13 08:14:24 -0300

    Consulta com vários critérios

--- src/main/java/com/marcelogarcia/workshopmongo/resources/PostResourse.java
+++ src/main/java/com/marcelogarcia/workshopmongo/resources/PostResourse.java
@@ -1,6 +1,7 @@
 package com.marcelogarcia.workshopmongo.resources;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,6 +38,18 @@ public class PostResourse {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value = "fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSerch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
